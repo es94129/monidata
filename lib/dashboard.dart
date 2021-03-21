93b9797f9f754f62abfd33bda4ed5948d13c3986
es94129 <es94129@gmail.com>
2021-03-22 01:28:23 +0800

    Add slider to dashboard

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class DashboardPage extends StatelessWidget {
+  final double _cpuCurrentUsage = 57;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -9,13 +11,26 @@ class DashboardPage extends StatelessWidget {
         children: <Widget>[
           SizedBox(height: 16,),
           Text(
-            "CPU usage: 58%",
+            "CPU usage: $_cpuCurrentUsage %",
             style: TextStyle(
                 fontWeight: FontWeight.bold,
                 color: Color(0xff75729e),
                 fontSize: 18
             ),
             textAlign: TextAlign.center,
+          ),
+          SliderTheme(
+              data: SliderTheme.of(context).copyWith(
+                trackHeight: 10,
+                disabledActiveTrackColor: Color(0x9900ad85),
+                thumbShape: RoundSliderThumbShape(enabledThumbRadius: 0.0), // hide thumb
+              ),
+              child: Slider(
+                value: _cpuCurrentUsage,
+                min: 0,
+                max: 100,
+                onChanged: null,
+              ),
           ),
         ],
       ),
