e3ee516f48a82608c26af89f90701e2926e0f013
es94129 <es94129@gmail.com>
2021-04-06 01:53:00 +0800

    Parse data chart json

--- lib/netdata/chart.dart
+++ lib/netdata/chart.dart
@@ -1,7 +1,21 @@
-class Charts {
+class Chart {
+  List<String> labels;
+  List<Data> data;
 
+  Chart fromJson(Map<String, dynamic> json) {
+    labels = (json['labels'] as List)?.map((e) => e as String)?.toList();
+    data = (json['data'] as List)?.map((e) {
+      return Data().fromJson(e);
+    })?.toList();
+    return this;
+  }
 }
 
-class Chart {
+class Data {
+  List<num> values;
 
+  Data fromJson(List<dynamic> json) {
+    values = json.map((e) => e as num)?.toList();
+    return this;
+  }
 }
--- lib/node_detail.dart
+++ lib/node_detail.dart
@@ -1,4 +1,10 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'netdata/chart.dart';
 
 class NodeDetailPage extends StatefulWidget {
   final String hostname;
@@ -10,22 +16,50 @@ class NodeDetailPage extends StatefulWidget {
 }
 
 class _NodeDetailState extends State<NodeDetailPage> {
+  Future<Chart> futureChart;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    futureChart = fetchChart(widget.hostname);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0x00ffffff),
-        elevation: 0,
-        title: Text(widget.hostname),
-        iconTheme: Theme.of(context).iconTheme,
-      ),
-      body: Text('Hello'),
+        appBar: AppBar(
+          backgroundColor: Color(0x00ffffff),
+          elevation: 0,
+          title: Text(widget.hostname),
+          iconTheme: Theme
+              .of(context)
+              .iconTheme,
+        ),
+        body: Center(
+            child: FutureBuilder<Chart>(
+              future: futureChart,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text(snapshot.data.labels[0]);
+                }
+                else if (snapshot.hasError) {
+                  return Text("${snapshot.error}");
+                }
+                return CircularProgressIndicator();
+              },
+            )
+        )
     );
+  }
+}
+
+Future<Chart> fetchChart(String hostname) async {
+  final response = await http.get(Uri.http("166.111.69.76:19999",
+      "/host/" + hostname + "/api/v1/data",
+      {"chart": "system.cpu"}));
+  if (response.statusCode == 200) {
+    return Chart().fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception("Failed to load info");
   }
 }
